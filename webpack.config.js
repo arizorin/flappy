a3bbfc75344e238eb594e09fea063bd181ddbfe4
Зорин Арсений <ZorinAL@pgkweb.ru>
2021-12-27 17:08:33 +0300

    Webpack config changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const appDirectory = fs.realpathSync(process.cwd());
 
 module.exports = {
@@ -34,6 +35,11 @@ module.exports = {
         new HtmlWebpackPlugin({
             inject: true,
             template: path.resolve(appDirectory, "public/index.html"),
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public' }
+            ]
         })
     ],
     mode: "development",
